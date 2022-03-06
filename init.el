50758ebf497ab9a772b6ff49014cbc0416dbed86
palkovics adam <pa.adam94@gmail.com>
2022-03-06 01:14:09 +0000

    autoinstall packages

--- init.el
+++ init.el
@@ -1,17 +1,31 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+;; init packages
+(setq package-archives
+        '(("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
+;; refresh contents on first start
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Declare packages
+(setq my-packages
+      '(which-key
+	magit))
+
+;; Iterate on packages and install missing ones
+(dolist (pkg my-packages)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(tango-dark))
- '(package-selected-packages
-   '(eglot use-package multiple-cursors go-mode magit rust-mode which-key))
+ '(package-selected-packages '(use-package multiple-cursors magit which-key))
  '(term-color-blue ((t (:background "dark cyan" :foreground "dark cyan")))))
 
+;; linum-mode
 (add-hook 'prog-mode-hook 'linum-mode)
 
 ;; backup in one place. flat, no tree structure
@@ -23,14 +37,10 @@
 ;; disable tool-bar
 (tool-bar-mode -1)
 
-;; Which-key
+;;;;;;;;;;;;;;;
+;; Which-key ;;
+;;;;;;;;;;;;;;;
 (which-key-mode 1)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; multiple cursors                                          ;;
@@ -42,3 +52,9 @@
 ;;;;;;;;;;;;;
 (global-set-key (kbd "C-c c") 'compile)
 (global-set-key (kbd "C-c r") 'recompile)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
