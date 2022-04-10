bfee632a5eb30a4c5d8b0dcc2b689f0974731273
palkovics adam <pa.adam94@gmail.com>
2022-04-10 13:59:21 +0200

    default face liberation-mono

--- .mc-lists.el
+++ .mc-lists.el
@@ -8,6 +8,7 @@
 	c-electric-delete-forward
 	c-electric-paren
 	c-indent-line-or-region
+	kill-region
 	))
 
 (setq mc/cmds-to-run-once
--- init.el
+++ init.el
@@ -24,9 +24,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(tango-dark))
+ '(font-use-system-font t)
  '(package-selected-packages
-   '(glsl-mode lsp-mode use-package multiple-cursors magit which-key))
- '(term-color-blue ((t (:background "dark cyan" :foreground "dark cyan")))))
+   '(rust-mode glsl-mode use-package multiple-cursors magit which-key))
+ '(term-color-blue ((t (:background "dark cyan" :foreground "dark cyan"))))
+ '(tool-bar-mode nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Liberation Mono" :foundry "1ASC" :slant normal :weight normal :height 98 :width normal)))))
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
 
 ;; linum-mode
 (add-hook 'prog-mode-hook 'linum-mode)
@@ -43,8 +54,19 @@
 ;; show-parenthesis
 (show-paren-mode 1)
 
-(global-set-key (kbd "C-c C-r") `replace-string)
-(global-set-key (kbd "C-c C-t") `query-replace)
+;; align
+(global-set-key (kbd "C-c C-a") 'align)
+(global-set-key (kbd "C-c C-s") 'align-regexp)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; generate ascending numbers	  ;;
+;; shorturl.at/vFGH0		  ;;
+;; 				  ;;
+;; with-appointed-amount-in-emacs ;;
+;; set-mark			  ;;
+;; type enter 90 times		  ;;
+;; C-u C-x r N Enter %3d Enter	  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;
 ;; Which-key ;;
@@ -66,14 +88,6 @@
 ;;;;;;;;;;;;;
 (global-set-key (kbd "C-c c") 'compile)
 (global-set-key (kbd "C-c r") 'recompile)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
 
 ;;;;;;;;;;;;;;;;;
 ;; winner-mode ;;
